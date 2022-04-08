1e4a9f81d0be2ac1351b431f9ec4cfe81e1b31df
taewok <taewok5163@naver.com>
2022-04-08 17:21:42 +0900

    onChange와 onClick으로 이벤트를 발생시켜보자

---
+++ README.md
@@ -0,0 +1,2 @@
+# React-Practice
+태욱이의 리액트 연습일지
--- event_handling/src/App.js
+++ event_handling/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import EventPractice from './EventPractice';
-import Test from './State_alert';
+import State_alert from './State_alert';
 
 function App() {
   return (
     <div className="App">
-      <Test></Test>
+      <State_alert></State_alert>
     </div>
   );
 }
