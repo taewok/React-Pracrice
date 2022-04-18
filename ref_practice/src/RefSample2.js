4b82f45e93b39162ed2a59196a68387b6f84a67d
taewok <taewok5163@naver.com>
2022-04-18 15:03:35 +0900

    버튼 클릭 시 스크롤이 맨 아래로 이동

--- ref_practice/src/RefSample2.js
+++ ref_practice/src/RefSample2.js
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 
 class RefSample2 extends Component {
     boxDown= () =>{
-        this.style1.style.width = '700px'
-        this.style1.style.transition = 'width ease 2s 0s'
+        this.style1.scrollTop = this.style1.scrollHeight- this.style1.clientHeight;
     }
     render() {
         const style1={
             width:'300px',
             height:'300px',
-            background:'red'
+            overflow:'auto'
         }
         const style2={
-            width:'200px',
-            height:'300px',
+            width:'100%',
+            height:'650px',
             background:'linear-gradient(red,blue)'
         }
         return (
-            <div>
-                <div style={style1} ref={(ref)=>this.style1=ref}></div>
+            <div style={style1} ref={(ref)=>this.style1=ref}>
                 <div style={style2}></div>
             </div>
         );
