5a30d2bc80a972166132328f9bf3a405563a158d
taewok <taewok5163@naver.com>
2022-04-22 18:52:18 +0900

    .

--- hooks_practice/src/App.js
+++ hooks_practice/src/App.js
@@ -1,12 +1,22 @@
+
+import { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
+import Info from './Info';
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className="App">
-      <Counter></Counter>
+    <div>
+      <button onClick={()=>{
+        setVisible(!visible);
+      }}>
+        {visible ? '숨기기': '보이기'}
+      </button>
+      <hr></hr>
+      {visible && <Info></Info>}
     </div>
-  );
+  )
 }
 
 export default App;
