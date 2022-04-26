129eb8490dbd1a2331351d72cdb6d4399e1a3e98
taewok <taewok5163@naver.com>
2022-04-26 19:40:41 +0900

    css의 SCSS 방식 연습

---
+++ styling_react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SassComponent from './SassComponent'
+
+
+function App() {
+  return (
+    <div>
+      <SassComponent></SassComponent>
+    </div>
+  );
+}
+
+export default App;
