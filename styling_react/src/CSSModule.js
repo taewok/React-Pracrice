9e731db6207db20edd7ae0d37ecd32f864193058
taewok <taewok5163@naver.com>
2022-04-27 15:36:39 +0900

    .

--- styling_react/src/CSSModule.js
+++ styling_react/src/CSSModule.js
@@ -4,7 +4,7 @@ import style from './CSSModule.module.css';
 const CSSModule = () => {
     console.log(style)
     return (
-        <div className={style.wrapper}>
+        <div className={`${style.wrapper} ${style.inverted}`}>
             안녕하세요 저는 <span className='something'>CSS Module!</span>
         </div>
     );
