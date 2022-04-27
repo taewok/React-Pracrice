82ec4847b78351c0715f5e7928d46870dda8ff84
taewok <taewok5163@naver.com>
2022-04-27 18:13:33 +0900

    .

--- styling_react/src/App.js
+++ styling_react/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component, useState } from 'react';
-import CSSModule from './CSSModule';
-import SassComponent from './SassComponent'
-import classNames from 'classnames';
-import './App.css';
+import React, { useState } from "react";
+import CSSModule from "./CSSModule";
+import SassComponent from "./SassComponent";
+import classNames from "classnames";
+import "./App.css";
+
 
 function App() {
-  const [clas,setClas] = useState(false);
+  const [clas, setClas] = useState(false);
+
   return (
     <div>
       <SassComponent></SassComponent>
-      <CSSModule/>
-      <div>
-        <span className={classNames('a',{clas})}>LOVE</span>
-        <button onClick={()=>{
-          setClas(true)
-        }}>클릭</button>
-      </div>
+      <CSSModule />
+      <span className={classNames("a", { clas })}>LOVE</span>
+      <button
+        onClick={() => {
+          setClas(true);
+        }}
+      >
+        클릭
+      </button>
     </div>
   );
 }
