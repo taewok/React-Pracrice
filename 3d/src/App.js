9bb58ec44e4f04add3d1e05f4df537d04953f174
taewok <taewok5163@naver.com>
2022-08-29 16:46:41 +0900

    루루 사진으로 3d 공간에서 원형으로 회전하는 이미지 완성

--- 3d/src/App.js
+++ 3d/src/App.js
@@ -1,20 +1,12 @@
-import { useEffect } from "react";
 import "./App.css";
+import Spin from "./Spin";
 
 function App() {
-  useEffect(()=>{
-    var card = document.querySelector(".card");
-    card.addEventListener("click", function () {
-      card.classList.toggle("is-flipped");
-    });
-  })
+  
   return (
-    <div className="card_box">
-      <div className="card">
-        <div className="card_face card_front">front</div>
-        <div className="card_face card_back">back</div>
-      </div>
-    </div>
+    <>
+      <Spin></Spin>
+    </>
   );
 }
 
