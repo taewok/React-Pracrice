06b8692031d74f10a0e1564672f79113849a4a6a
taewok <taewok5163@naver.com>
2022-10-24 18:10:48 +0900

    mouse 이동 기능 마우스 주변은 동그랗게 배경이 선명해진다

--- css_practice/src/Component/Background.js
+++ css_practice/src/Component/Background.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../SCSS/Background.scss"
 
 const Background = () => {
+    useEffect(()=>{
+        let focus = document.querySelector(".focus");
+      let focusHalfWidth = focus.offsetWidth/2;
+      document.onmousemove = (e)=>{
+        focus.style.left = e.pageX - focusHalfWidth+"px";
+        focus.style.top = e.pageY - focusHalfWidth+"px";
+      }
+      })
     return (
         <div className='hero'>
             <h1>Highlights</h1>
