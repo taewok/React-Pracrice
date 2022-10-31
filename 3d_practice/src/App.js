050c5039d51bb34825512009c5ba6f6a1bfb7036
taewok <taewok5163@naver.com>
2022-11-01 04:14:34 +0900

    폴더 이름 변경

---
+++ 3d_practice/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PostEffect from "./component/PostEffect";
+
+function App() {
+  
+  return (
+    <>
+      <PostEffect/>
+    </>
+  );
+}
+
+export default App;
---
+++ 3d_practice/src/component/Spin.js
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useEffect } from "react";
+import "../Css/Spin.css";
+
+const Spin = () => {
+  useEffect(()=>{
+    const imgSpan = document.querySelectorAll(".box span");
+    for (let index = 0; index < imgSpan.length; index++) {
+      imgSpan[index].style.transform = `rotateY(calc(${index+1} * 45deg)) translateZ(400px)`
+    }
+  })
+  return (
+    <>
+      <div className="box">
+        <span>
+          <img src="1.jpg"></img>
+        </span>
+        <span>
+          <img src="2.jpg"></img>
+        </span>
+        <span>
+          <img src="3.jpg"></img>
+        </span>
+        <span>
+          <img src="4.jpg"></img>
+        </span>
+        <span>
+          <img src="5.jpg"></img>
+        </span>
+        <span>
+          <img src="6.jpg"></img>
+        </span>
+        <span>
+          <img src="7.jpg"></img>
+        </span>
+        <span>
+          <img src="8.jpg"></img>
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Spin;
